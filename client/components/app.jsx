bc871cdfe921b6a5e4e83beeab7bf2a7014445e0
Michael Kim <miketkim@umich.edu>
2019-08-29 17:02:33 -0700

    fixed arrows

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -8,8 +8,8 @@ class App extends Component {
     super(props);
     this.state = {
       places: [],
-      fadedleft: true,
-      fadedright: false,
+      // fadedleft: true,
+      // fadedright: false,
       start: 0,
       finish: 3
     }
@@ -36,14 +36,15 @@ class App extends Component {
         start: start - 1,
         finish: finish - 1,
       });
-    } else {
-      this.setState({
-        fadedleft: true
-      });
-    }
-    this.setState({
-      fadedright: false
-    })
+    } 
+    //else {
+    //   this.setState({
+    //     fadedleft: true
+    //   });
+    // }
+    // this.setState({
+    //   fadedright: false
+    // })
   }
   rightClick() {
     let start = this.state.start;
@@ -53,33 +54,33 @@ class App extends Component {
         start: start + 1,
         finish: finish + 1
       });
-    } else {
-      this.setState({
-        fadedright: true
-      });
-    }
+    } //else {
+    //   this.setState({
+    //     fadedright: true
+    //   });
+    // }
     
-    this.setState({
-      fadedleft: false
-    });
+    // this.setState({
+    //   fadedleft: false
+    // });
   }
   render() {
     var startindex = this.state.start;
     var finishindex = this.state.finish;
-    const fadedleft = this.state.fadedleft ? "arrow-left faded-left" : "arrow-left";
-    const fadedright = this.state.fadedright ? "arrow-right faded-right" : "arrow-right";
+    // const fadedleft = this.state.fadedleft ? "arrow-left faded-left" : "arrow-left";
+    // const fadedright = this.state.fadedright ? "arrow-right faded-right" : "arrow-right";
     
     return (
       <React.Fragment>
       <h1 className='h1' tabIndex='-4'>More places to stay</h1>
       <div className="container">
         <div className="slideshow row col-md-12">
-          <div className={fadedleft} onClick={this.leftClick.bind(this)}></div>
+          <div className='arrow-left' onClick={this.leftClick.bind(this)}></div>
           {
             this.state.places.slice(startindex, finishindex).map(place => 
             <PlacesCard key={place.id} place={place} />)
             }
-          <div className={fadedright} onClick={this.rightClick.bind(this)}></div>
+          <div className='arrow-right' onClick={this.rightClick.bind(this)}></div>
         </div>
       </div>
       </React.Fragment>
